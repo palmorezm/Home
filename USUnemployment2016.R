a1cdb25466d94be4762c1ad668c2c30bd4c0b525
palmorezm <zachary.palmore20@spsmail.cuny.edu>
2021-10-04 16:51:13 -0400

    Create USUnemployment2016.R
    Choropleth map by county using GeoFips

---
+++ USUnemployment2016.R
@@ -0,0 +1,40 @@
+
+# GeoFips Mapping Example
+# Choropleth of US Unemployment by County
+
+library(rjson)
+library(plotly)
+
+url <- 'https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json'
+counties <- rjson::fromJSON(file=url)
+url2<- "https://raw.githubusercontent.com/plotly/datasets/master/fips-unemp-16.csv"
+df <- read.csv(url2, colClasses=c(fips="character"))
+g <- list(
+  scope = 'usa',
+  projection = list(type = 'albers usa'),
+  showlakes = TRUE,
+  lakecolor = toRGB('white')
+)
+fig <- plot_ly()
+fig <- fig %>% add_trace(
+  type="choropleth",
+  geojson=counties,
+  locations=df$fips,
+  z=df$unemp,
+  colorscale="Viridis",
+  zmin=0,
+  zmax=12,
+  marker=list(line=list(
+    width=0)
+  )
+)
+fig <- fig %>% colorbar(title = "Unemployment Rate (%)")
+fig <- fig %>% layout(
+  title = "2016 US Unemployment by County"
+)
+
+fig <- fig %>% layout(
+  geo = g
+)
+
+fig
