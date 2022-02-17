3615d621fb3f7994b500fdc864fc205e4ab1a675
palmorezm <zachary.palmore20@spsmail.cuny.edu>
2022-02-16 18:30:04 -0600

    Create LockScreenLocations.R
    The link comes from a google docs published. It contains all the lock screen locations that have appeared on my computer that I liked well enough to save. Further parsing is needed.

---
+++ Vacation/LockScreenLocations.R
@@ -0,0 +1,7 @@
+
+# Lock Screen Locations
+# Web-searching List
+# Published via Glink
+
+file <- "https://docs.google.com/document/d/e/2PACX-1vSrVkVvQBE75wh9Pb095eY5WphbaclpuS5dzwfqII8Ht4e9hY_Pu5NHc_6P3xJI7qBqjzmLmfmBAmm8/pub"
+locations <- read.delim(file)
