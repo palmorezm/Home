ae87ce46210569a2a890f59ac15fdef19f5d3b2a
palmorezm <zachary.palmore20@spsmail.cuny.edu>
2022-03-15 21:31:48 -0500

    Review dataprep.R and Add Notes
    Reviewed what was needed in the dataprep section after some basic cleaning. It is clear that the location variable is not useful as a character string without a lot of regular expressions. We should consider factorizing it. We also should provide dollar amounts when possible, fix misclassified points, and form key-value pairs while trying to answer the big picture question: What do we want to gain from this?

--- Financial/dataprep.R
+++ Financial/dataprep.R
@@ -19,4 +19,14 @@ df$Account <- as.factor(df$Account)
 df <- df %>% 
   dplyr::select("Location", "Date", "Description", 
                 "Cost", "Category", "Account") 
-plot(df)
+summary(df)
+
+hist(df$Cost) # Needs to be fixed, current bin size is 0 - 50 with 800+ transactions
+as.Date.character() # Does this work to format the date or will we need to change the source data type? 
+# Location - Should this be a factor data type with lots of levels? We shop at a lot of the same stores
+# Category - needs an amount associated with each
+# Amount - needs an amount associated with each and needs fixing (extra 11 transactions under Zach
+#   would otherwise be unclassified)
+# Big picture - what do we want to gain from this as a shiny?
+#   Are there any particular questions that we ask each time we want to make a purchase that could save us money? 
+
